65b1b28e0ef4bcdc733b30261e5fb173b60c05b1
Torri <vika.silanteva.05@list.ru>
2023-11-12 20:24:28 +0300

    fin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+from random import randint
 with open('books.csv', newline='') as File:  
     reader = csv.reader(File)
     print("task1")
@@ -13,6 +14,7 @@ with open('books.csv', newline='') as File:
                 #print(nazv,len(nazv))
                 kol+=1
 print(kol)
+
 print("task2")
 print("Введите автора")
 avtor=input()
@@ -25,3 +27,11 @@ with open('books.csv', newline='') as File:
         if i>1:
             if (row.split(";")[3]==avtor) and (float(row.split(";")[7])>=150.0):
                 print(row.split(";")[1])
+                
+print("task3")
+with open('books.csv', newline='') as File:
+    reader = list(csv.DictReader(File, delimiter=';'))
+    bibl = open("bibliograf.txt", "w")
+    for i in range( 20):    
+        number = randint(1, 9400)
+        bibl.write(f"{i+1} {reader[number]['Автор']} - {reader[number]['Название']} - год \n") #в таблице нет дат
